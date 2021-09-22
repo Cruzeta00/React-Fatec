63af23f0d35ce9da164b0c03af08c9604708cfd1
Júlio César <julio10as@gmail.com>
2021-09-22 11:23:37 -0300

    esqueci de subir

--- CredoAlexandre/feed-do-reddit/src/index.js
+++ CredoAlexandre/feed-do-reddit/src/index.js
@@ -1,51 +1,50 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card'
-import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
 import * as Icon from 'react-bootstrap-icons'
 
 export default class App extends React.Component {
     render() {
         return (
             <Container className="mt-2">
-                <Row className="justify-content-left">
-                    <Col md={8}>
-                        <Card >
-                            <Card.Header as="h5" className="text-center">
-                                Post do Dia
-                            </Card.Header>
-                            <Card.Body
-                                className="d-flex flex-column justify-content-center
-                                align-items-center"
-                                style={{height: '8rem'}}>
-                                <div className="Votacao ">
+                    <div md={8} className="card row">
+                        <div className="d-flex ">
+                            <div className="col-xs-6">
+                                <p>UPVOTE</p>
+                                <p>DOWNVOTE</p>
+                            </div>
+                            <div className="col-xs-6">
+                                <div as="h5" className="text-center card-header">
+                                    Post do Dia
                                 </div>
-                                AQUI TA A IMAGEM DO POST
-                            </Card.Body>
-                            <Card.Footer className="d-flex flex-row justify-content-center
-                                align-items-center"
-                                >
-                                <Button className="mt-2 " onClick={this.upVote}>
-                                    <Icon.ArrowUpCircleFill class=""/>
-                                </Button>
-                                <Button variant="outline-success" className="w-15 mt-2" onClick={this.downVote}>
-                                    <Icon.ArrowUpCircleFill class="aqua-blue size-medium "/>
-                                </Button>
-                                <Button variant="outline-success" className="w-15 mt-2" onClick={this.upVote}>
-                                    <Icon.ArrowUpCircleFill class="color: aqua-blue size-medium"/>   
-                                </Button>
-                                <Button variant="outline-success" className="w-15 mt-2" onClick={this.upVote}
-                                style={{class:"fas fa-chevron-up"}}>
-                                    <Icon.ArrowUpCircleFill class="color: aqua-blue size-medium"/>
-                                </Button>
-                            </Card.Footer>
-                        </Card>
-                    </Col>
-                </Row>
+                                <div className="d-flex flex-column justify-content-center
+                                    align-items-center card-body"
+                                    style={{height: '8rem'}}>
+                                    <div className="Votacao ">
+                                    </div>
+                                    AQUI TA A IMAGEM DO POST
+                                </div>
+                                <ButtonGroup className="d-flex container-fluid card-footer" size="sm">
+                                    <Button variant="dark" className="mt-2 " onClick={this.upVote}>
+                                        <Icon.ArrowUpCircleFill/>
+                                    </Button>
+                                    <Button variant="blue" className="w-15 mt-2 ml-88" onClick={this.downVote}>
+                                        <Icon.ArrowDownCircleFill/>
+                                    </Button>
+                                    <Button variant="outline-success" className="w-15 mt-2" onClick={this.upVote}>
+                                        <Icon.ArrowUpCircleFill class="color: aqua-blue size-medium"/>   
+                                    </Button>
+                                    <Button variant="outline-success" className="w-15 mt-2" onClick={this.upVote}
+                                    style={{class:"fas fa-chevron-up"}}>
+                                        <i class="BsFillAwardFill"></i>
+                                    </Button>
+                                </ButtonGroup>
+                            </div>
+                        </div>
+                    </div>
             </Container>
         )
     }
---
+++ Semana6/Cartao.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Cartao = (props) => {
+    return (
+        <div>
+            {props.children}
+        </div>
+    )
+}
+
+export default Cartao
+
+const styles = StyleSheet.create({
+    
+})
